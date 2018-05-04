589e5569fda744a1a9a1f6f20d26479a16095ba5
Peefy <xpf6677@163.com>
2018-05-04 09:20:08 +0800

    Add cpp11

--- main.cpp
+++ main.cpp
@@ -26,17 +26,17 @@ int main()
     {
         cout << num << " ";
     }
-    //std::cout << [](float f) -> int { return std::abs(f); } (-3.5);
-    //auto lambda = [] (float val) -> int { return val * 100; };
+    std::cout << [](float f) -> int { return std::abs(f); } (-3.5);
+    auto lambda = [] (float val) -> int { return val * 100; };
     float f0 = 1.0;
-    //std::cout << [=](float f) mutable { return f0 += std::abs(f); } (-3.5);
+    std::cout << [=](float f) mutable { return f0 += std::abs(f); } (-3.5);
     std::cout << '\n' << f0 << '\n';
     cout << endl;
     cout << "hello world" << endl;
     cout << "hello world" << endl;
-    //ChinaDate* chinadate = new ChinaDate;
-    //cout << chinadate->GetLongSecond() << endl;
-    //getchar();
+    ChinaDate* chinadate = new ChinaDate;
+    cout << chinadate->GetLongSecond() << endl;
+    getchar();
     return 0;
 }
 
